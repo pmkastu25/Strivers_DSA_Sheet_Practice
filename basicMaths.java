b4891ac8c8e74d76c73a230a8280a6edf56dd697
pmkastu25 <pmkastu25@gmail.com>
2026-07-24 20:58:39 +0530

    Solved basic Math problems from the sheet

---
+++ basicMaths.java
@@ -0,0 +1,75 @@
+
+public class basicMaths {
+    public static int countDigitsofNum(int num){
+        int count = 0;
+        while(num != 0){
+            count++;
+            num /= 10;
+        }
+
+        return count;
+    }
+
+    public static int reverseNumber(int num){
+        int revNum = 0;
+        while(num != 0){
+            int ld = num % 10;
+            revNum = revNum * 10 + ld;
+            num /= 10;
+        }
+
+        return revNum;
+    }
+
+     public static boolean isPalindromeNumber(int num){
+        int origNum = num;
+        int revNum = 0;
+        while(num != 0){
+            int ld = num % 10;
+            revNum = revNum * 10 + ld;
+            num /= 10;
+        }
+
+        if(origNum != revNum){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isArmstrongNumber(int num){
+        int orgNum = num;
+        int armNum = 0;
+        int numDigits = countDigitsofNum(num);
+        while(num != 0){
+            int ld = num % 10;
+            int newNum = (int)Math.pow(ld, numDigits);
+            armNum += newNum;
+            num /= 10;
+        }
+
+        return ((armNum == orgNum) ? true : false);
+    }
+
+    public static void printAllDivisors(int num){
+        for(int i=1; i<=num; i++){
+            if(num % i == 0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Count the Digits of the Number: "+countDigitsofNum(125467));
+
+        System.out.println("Reverse of a Number: "+reverseNumber(125467));
+
+        System.out.println("Number is a Palindrome?: "+isPalindromeNumber(4554));
+        System.out.println("Number is a Palindrome?: "+isPalindromeNumber(7785));
+
+        System.out.println("Number is an Armstrong Num?: "+isArmstrongNumber(153));
+        System.out.println("Number is an Armstrong Num?: "+isArmstrongNumber(421));
+
+        System.out.println("Printing all the Divisors: ");
+        printAllDivisors(36);
+    }
+}
