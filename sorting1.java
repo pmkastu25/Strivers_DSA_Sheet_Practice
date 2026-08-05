90052ccbbd0bd947739dc668fcd13163c3813f45
pmkastu25 <pmkastu25@gmail.com>
2026-08-05 22:08:02 +0530

    Solved bubble sort using brute force

---
+++ sorting1.java
@@ -0,0 +1,26 @@
+public class sorting1 {
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void bubbleSort(int arr[]){
+        int n = arr.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {2,7,5,6};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
