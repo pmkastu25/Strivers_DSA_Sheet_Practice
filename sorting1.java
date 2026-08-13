ddbb83dbe14c3f4e721bd9107ec0029cec7a68ac
pmkastu25 <pmkastu25@gmail.com>
2026-08-13 09:52:39 +0530

    Solved Selection Sort

--- sorting1.java
+++ sorting1.java
@@ -18,9 +18,32 @@ public class sorting1 {
             }
         }
     }
+    public static void selectionSort(int arr[]){
+        int n = arr.length;
+
+        for(int i=0; i<n-1; i++){
+            int minIdx = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[minIdx]){
+                    minIdx = j;
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+        }
+    }
+
     public static void main(String[] args){
         int arr[] = {2,7,5,6};
         bubbleSort(arr);
         printArr(arr);
+
+        System.out.println();
+
+        int arr1[] = {3,1,7,4};
+        selectionSort(arr1);
+        printArr(arr1);
     }
 }
